1a64b467e41627c3acd51a9c846048b6171dc9f2
Masum-developer-python <developer.rahmatullah.masum@gmail.com>
2025-05-27 22:35:25 +0600

    modified:   src/components/LetterCard.jsx

--- src/components/LetterCard.jsx
+++ src/components/LetterCard.jsx
@@ -24,7 +24,7 @@ function Cards({
     <>
       <div key={rowIndex} className="flex flex-wrap w-[100%] space-x-1 m-1 ">
         <div
-          className={`font-bangla flex  justify-center items-center  text-center text-2xl w-[100%] m-2 max-h-[40px] ${selectedColor.backgroundColor} ${selectedColor.textColor}`}
+          className={`font-bangla flex  justify-center items-center text-center text-2xl relative left-6 w-[calc(100%-30px)] md:left-0 md:w-[100%] m-2 max-h-[40px] ${selectedColor.backgroundColor} ${selectedColor.textColor}`}
         >
           <span className="text-3xl text-center">
             আরবী বর্ণমালা <span className="text-3xl">{title} </span>
@@ -49,13 +49,13 @@ function Cards({
           />
         </div>
       )}
-        <div className="flex flex-0 flex-wrap flex-row-reverse w-full my-auto md:m-2">
+        <div className="flex flex-0 flex-wrap flex-row-reverse w-full m-auto md:m-2">
           {arabicAlphabet
             .filter((row) => row.extra != 1)
             .map((item, itemIndex) => (
               <div
                 key={`container-${rowIndex}-${itemIndex}`}
-                className=" group flex-grow "
+                className=" group flex-grow"
               >
                 <Audio
                   folder="/alphabets/"
@@ -63,7 +63,7 @@ function Cards({
                 >
                   <div
                     key={`item-${rowIndex}-${itemIndex}`}
-                    className={`rtl p-1 py-2 m-auto my-2 w-[150px] md:w-64 h-auto
+                    className={`rtl p-1 py-2 m-auto my-2 w-[150px] md:w-64 ${withHoverChildren ? "":"h-32"} md:h-48
             ${selectedColor.backgroundColor} 
             text-8xl text-center 
             ${selectedColor.textColor} rounded-lg`}
